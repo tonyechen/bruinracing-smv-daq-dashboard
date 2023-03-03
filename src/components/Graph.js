02466373b85a44e88edc02bbeed9071a20fc052b
Anthony Chen <88166446+tonyechen@users.noreply.github.com>
2023-03-03 00:13:27 -0800

    built graph prototypes

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { Box, Button } from '@mui/material';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
 
 const Graph = () => {
-  return <div>Graph</div>;
+    // const [liveData, setLiveData] = useState([65, 59, 80, 81, 56, 55, 40]);
+
+    const labels = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    const data = {
+        labels: labels,
+        datasets: [
+            {
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.2,
+            },
+        ],
+    };
+
+    const options = {
+        responsive: true,
+        aspectRatio: 1,
+        interaction: {
+            intersect: false,
+        },
+    };
+
+    const chartRef = useRef(null);
+
+    const addData = () => {
+        const chart = chartRef.current;
+        chart.data.labels.push(parseInt(Math.random() * 10));
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.push(Math.random() * 20 + 50);
+        });
+        chart.update();
+    };
+    return (
+        <>
+            <Line
+                className='graph'
+                ref={chartRef}
+                data={data}
+                options={options}
+            ></Line>
+            <Button onClick={addData}>Add Data</Button>
+        </>
+    );
 };
 
 export default Graph;
--- src/pages/LiveInterface.js
+++ src/pages/LiveInterface.js
@@ -1,12 +1,18 @@
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import React from 'react';
+import Graph from '../components/Graph';
 import Map from '../components/Map';
 
 const LiveInterface = () => {
     return (
         <Grid container sx={{ width: '100%', height: '90%', overflow: 'auto' }}>
             <Grid item xs={12} md={12} sx={{ height: '30%' }}>
-                Graphs
+                <Box sx={{ height: '100%', width: '100%', display: 'flex' }}>
+                    <Graph />
+                    <Graph />
+                    <Graph />
+                    <Graph />
+                </Box>
             </Grid>
             <Grid item xs={4} md={4} sx={{ height: '70%' }}>
                 Trial Data?
